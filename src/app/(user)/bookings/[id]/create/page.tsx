10acaf4d35ec9edefeab65dca2f4736c110a0c2d
applestz <aimpawee8524@gmail.com>
2026-03-24 19:55:38 +0700

    add padding and margin to create booking

--- src/app/(user)/bookings/[id]/create/page.tsx
+++ src/app/(user)/bookings/[id]/create/page.tsx
@@ -12,11 +12,17 @@ export default async function CreateBookingPage({params}: {params: Promise<{id: 
     const carRental = await getCarRentalById(id)
 
     return (
-        <main>
-            <h1 className="text-[var(--color-second-purple)] text-3xl font-bold">
-                Create Your Bookings
+        <main className="mx-auto max-w-5xl px-4 sm:px-6 py-10">
+
+            <h1 className="text-center text-3xl sm:text-4xl font-bold text-[var(--color-second-purple)] my-10">
+                Create Your Booking
             </h1>
-            <CreateBookingForm token={session.user.token} carRentalItem={carRental}/>
+
+            <CreateBookingForm 
+                token={session.user.token} 
+                carRentalItem={carRental}
+            />
+
         </main>
     )
 }
